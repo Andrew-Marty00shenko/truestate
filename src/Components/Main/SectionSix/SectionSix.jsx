d0daa686aa415db33e69e7e38bf13a6a49a4b058
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2022-07-24 22:23:27 +0300

    translate section six

--- src/Components/Main/SectionSix/SectionSix.jsx
+++ src/Components/Main/SectionSix/SectionSix.jsx
@@ -1,44 +1,47 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 
 import "./SectionSix.scss";
 
 const SectionSix = () => {
+    const { t } = useTranslation();
+
     return <section className="section-six">
         <h2>
-            Как Это Работает?
+            {t('landing:SECTION_SIX_TITLE')}
         </h2>
         <Container fluid>
             <Row className="justify-content-between">
                 <Col lg={6} className="section-six__item">
                     <h3>
-                        Шаг 1.
+                        {t('landing:SECTION_SIX_STEP')} 1.
                     </h3>
                     <p>
-                        Выбери объект на сайте, отправь сумму в ETH со своего кошелька Metamask на указанный под ним адрес.
+                        {t('landing:SECTION_SIX_STEP_1_TEXT')}
                     </p>
                 </Col>
                 <Col lg={6} className="section-six__item">
                     <h3>
-                        Шаг 2.
+                        {t('landing:SECTION_SIX_STEP')} 2.
                     </h3>
                     <p>
-                        Получи токены TRUESTATE и добавь их в свой кошелёк Metamask (инструкция - <span>здесь</span>).
+                        {t('landing:SECTION_SIX_STEP_2_TEXT')} <span>{t('landing:SECTION_SIX_STEP_2_HREF')}</span>).
                     </p>
                 </Col>
                 <Col lg={6} className="section-six__item">
                     <h3>
-                        Шаг 3.
+                        {t('landing:SECTION_SIX_STEP')} 3.
                     </h3>
                     <p>
-                        После окончания проекта получи уведомление через email и / или смс.
+                        {t('landing:SECTION_SIX_STEP_3_TEXT')}
                     </p>
                 </Col>
                 <Col lg={6} className="section-six__item">
                     <h3>
-                        Шаг 4.
+                        {t('landing:SECTION_SIX_STEP')} 4.
                     </h3>
                     <p>
-                        Зайди в личный кабинет на сайте и получи возврат инвестиций и прибыль (инструкция - <span>здесь</span>).
+                        {t('landing:SECTION_SIX_STEP_4_TEXT')} <span>{t('landing:SECTION_SIX_STEP_4_HREF')}</span>).
                     </p>
                 </Col>
             </Row>
