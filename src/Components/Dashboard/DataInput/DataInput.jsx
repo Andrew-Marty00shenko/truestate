394737f848aa15fade14ca2580b038eca24b3473
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2022-08-11 23:49:57 +0300

    add button and text in data input page

--- src/Components/Dashboard/DataInput/DataInput.jsx
+++ src/Components/Dashboard/DataInput/DataInput.jsx
@@ -106,6 +106,21 @@ const DataInput = () => {
                 </Button>
             </ButtonGroup>
         </div>
+
+        <div className="data-input__send-btn">
+            <Button
+                className="send-btn"
+            >
+                {t('dataInput:SEND_BTN')}
+                <svg width="36" height="13" viewBox="0 0 36 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M35.2492 7.10782C35.5849 6.77213 35.5849 6.22787 35.2492 5.89218L29.7788 0.421787C29.4431 0.0860968 28.8989 0.0860968 28.5632 0.421787C28.2275 0.757478 28.2275 1.30174 28.5632 1.63743L33.4257 6.5L28.5632 11.3626C28.2275 11.6983 28.2275 12.2425 28.5632 12.5782C28.8989 12.9139 29.4431 12.9139 29.7788 12.5782L35.2492 7.10782ZM0.257812 7.35959H34.6414V5.64041H0.257812V7.35959Z" fill="white" />
+                </svg>
+            </Button>
+
+            {residentOfUSA.yes ? <p>
+                *Вы не можете покупать токены TRUESTATE.
+            </p> : ""}
+        </div>
     </form>
 }
 
