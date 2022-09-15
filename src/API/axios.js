9bed67464cd1470e80c790eb3b55e10dfed5a7b4
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2022-09-15 20:57:37 +0300

    Update axios.js

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = window.location.origin;
+axios.defaults.baseURL = 'http://topmail.net.ua:8089';
 axios.defaults.headers.common['access_token'] = window.localStorage.access_token;
 
 window.axios = axios;
