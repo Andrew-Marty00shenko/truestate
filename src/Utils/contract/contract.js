c5694e99151ed0356564f5fb7aad27b274167d57
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2022-10-11 23:18:16 +0300

    Update contract.js

--- src/Utils/contract/contract.js
+++ src/Utils/contract/contract.js
@@ -1,7 +1,6 @@
 import Web3 from "web3";
-import { contractABI } from "./contractABI";
-
 import transactionAPI from "../../API/transactionsAPI";
+const { contractABI } = require("./contractABI");
 
 export const ETH_RPC = 'https://rpc-mumbai.maticvigil.com/';
 export const HASH_LINK = 'https://mumbai.polygonscan.com/';
