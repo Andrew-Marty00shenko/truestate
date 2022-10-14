fd940bc1bcf80d745b42fa2feb948835f773abd7
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2022-10-14 16:07:13 +0300

    Update ConnectWalletButton.jsx

--- src/Components/Dashboard/ConnectWalletButton/ConnectWalletButton.jsx
+++ src/Components/Dashboard/ConnectWalletButton/ConnectWalletButton.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import { Button } from "react-bootstrap"
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux/es/hooks/useSelector";
+import { connectWallet } from "../../../Utils/contract/contract";
 
 import "./ConnectWalletButton.scss";
 
 const ConnectWalletButton = () => {
     const { t } = useTranslation();
     const checkingResidentOfUsa = useSelector(state => state.checkingResidentOfUsa.value);
+    const [addressWallet, setAddressWallet] = useState(null);
 
-    return <Button className="connect-wallet-btn" disabled={checkingResidentOfUsa || window.location.pathname === '/dashboard/login'}>
-        {t('dashboardMain:DASHBOARD_CONNECT_WALLET')}
+    const handleClickConnectWallet = async () => {
+        const account = await connectWallet();
+        setAddressWallet(account);
+    };
+
+    return <Button className="connect-wallet-btn"
+        onClick={handleClickConnectWallet}
+        disabled={checkingResidentOfUsa || window.location.pathname === '/dashboard/login'}
+    >
+        {addressWallet === null
+            ? t('dashboardMain:DASHBOARD_CONNECT_WALLET')
+            : `${addressWallet.substr(0, 8)
+            + '...'
+            + addressWallet.substr(addressWallet.length - 8, addressWallet.length)
+            }`}
     </Button>
 }
 
